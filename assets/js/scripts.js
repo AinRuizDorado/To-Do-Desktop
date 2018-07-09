1671345dd849fba81072017f7241787c92d58688
maxain <maxain201@gmail.com>
2018-07-09 20:12:52 -0300

    mini style, some code jquery assets

---
+++ assets/js/scripts.js
@@ -0,0 +1,26 @@
+
+// Mark to do list done
+$("ul").on("click", "li", function(e){
+    $(this).toggleClass( "line-through" )
+});
+
+// Delete To Do list
+$("ul").on("click", "span",function(e){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
+
+
+$("input[type = 'text']").on("keypress", function(e){
+    if (e.which === 13) {
+        // Grabbing new TO DO
+      let todoText = $(this).val();
+    //   clear input
+      $(this).val("");
+    //   pushing to new LI TO DO
+    $("ul").append("<li> <span> X </span>"+ todoText + "</li>");
+        
+    }
+});
